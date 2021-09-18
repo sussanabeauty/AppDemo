853d743f600f23ffc85d2f3abef11be9dd7cb55e
Sussana Kwabi <susannabeautyk@gmail.com>
2021-09-18 00:15:46 -0500

    added activity

---
+++ app/src/main/java/org/sussanacode/myapplication/MainActivity.kt
@@ -0,0 +1,15 @@
+package org.sussanacode.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+
+// git Token: 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
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
