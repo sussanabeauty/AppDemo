81c03599cfe194d126de29a6292b3bf6ad14468a
Sussana Kwabi <susannabeautyk@gmail.com>
2021-09-18 00:22:06 -0500

    Toast message to emulator

--- app/src/main/java/org/sussanacode/myapplication/MainActivity.kt
+++ app/src/main/java/org/sussanacode/myapplication/MainActivity.kt
@@ -2,6 +2,7 @@ package org.sussanacode.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 
 // git Token: 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
@@ -13,5 +14,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         println("system out put")
+
+        Toast.makeText(baseContext, "Printing statement when app is launched on the emulator", Toast.LENGTH_LONG).show()
     }
 }
